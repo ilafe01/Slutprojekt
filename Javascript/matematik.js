34d74f349143d8eb92cd0fe82b5ca7ee80e7973a
ilafe01 <ilanthamer2008@gmail.com>
2026-05-17 02:32:39 +0200

    Skapat quiz i javascript

---
+++ Javascript/matematik.js
@@ -0,0 +1,88 @@
+let fråga1A = document.getElementById("Fråga1A");
+let fråga1B = document.getElementById("Fråga1B");
+let fråga1C = document.getElementById("Fråga1C");
+
+let fråga2A = document.getElementById("Fråga2A");
+let fråga2B = document.getElementById("Fråga2B");
+let fråga2C = document.getElementById("Fråga2C");
+
+let fråga3A = document.getElementById("Fråga3A");
+let fråga3B = document.getElementById("Fråga3B");
+let fråga3C = document.getElementById("Fråga3C");
+
+let resultat = document.getElementById("resultat");
+
+let poäng = 0;
+
+fråga1A.addEventListener("click" , function () {
+
+    resultat.innerHTML = "Rätt svar!";
+    resultat.style.color = "green";
+
+    poäng++:
+    
+});
+
+fråga1B.addEventListener("click" , function () {
+
+    resultat.innerHTML = "Fel svar!";
+    resultat.style.color = "red";
+
+});
+
+fråga1A.addEventListener("click" , function () {
+
+    resultat.innerHTML = "Fel svar!";
+    resultat.style.color = "red";
+    
+});
+
+fråga2A.addEventListener("click" , function () {
+
+    resultat.innerHTML = "Fel svar!";
+    resultat.style.color = "red";
+    
+});
+
+fråga2B.addEventListener("click" , function () {
+
+    resultat.innerHTML = "Rätt svar!";
+    resultat.style.color = "green";
+
+    poäng++:
+    
+});
+
+fråga2C.addEventListener("click" , function () {
+
+    resultat.innerHTML = "Fel svar!";
+    resultat.style.color = "red";
+
+});
+
+fråga3A.addEventListener("click" , function () {
+
+    resultat.innerHTML = "Fel svar!";
+    resultat.style.color = "red";
+    
+});
+
+fråga3B.addEventListener("click" , function () {
+
+    resultat.innerHTML = "Rätt svar!";
+    resultat.style.color = "green";
+
+    poäng++:
+    
+});
+
+fråga3C.addEventListener("click" , function () {
+
+    resultat.innerHTML = "Fel svar!";
+    resultat.style.color = "red";
+    
+});
+
+
+
+
